e6f73a15746e895b0b738c667711fe1a91b9f846
sergiioo101 <smartros@myuax.com>
2023-12-12 12:14:43 +0100

    Definición del alias para la estructura Estudiante

---
+++ Creación de alias de tipos de estructuras.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+// Definición del alias para la estructura Estudiante
+typedef struct Estudiante {
+    char nombre[50];
+    int edad;
+    float promedio;
+} Estudiante;
