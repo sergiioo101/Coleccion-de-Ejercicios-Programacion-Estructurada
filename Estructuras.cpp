6fde037e32151ed359f83e2cb650e177ed5a5ec8
sergiioo101 <smartros@myuax.com>
2023-12-12 11:53:20 +0100

    apartado c)

--- Estructuras.cpp
+++ Estructuras.cpp
@@ -18,3 +18,8 @@ int main() {
     strcpy(estudiante1.nombre, "Juan");
     estudiante1.edad = 20;
     estudiante1.promedio = 9.5;
+    // c. Instanciación con el operador malloc
+    struct Estudiante *estudiante2 = (struct Estudiante *) malloc(sizeof(struct Estudiante));
+    strcpy(estudiante2->nombre, "Ana");
+    estudiante2->edad = 22;
+    estudiante2->promedio = 9.8;
