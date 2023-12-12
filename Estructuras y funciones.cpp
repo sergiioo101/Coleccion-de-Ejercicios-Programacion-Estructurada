2cf35d14375c8a1e4dbad8b1c2358b0b35714a36
sergiioo101 <smartros@myuax.com>
2023-12-12 12:24:34 +0100

    funcion main

--- Estructuras y funciones.cpp
+++ Estructuras y funciones.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <cstdio>
 
 // Definición de la estructura Estudiante
 typedef struct {
@@ -17,4 +17,25 @@ void modificarEstudiante(Estudiante *est) {
 // c. Pasar una estructura por dirección como argumento
 void imprimirEstudiante2(Estudiante *est) {
     printf("Nombre: %s, Edad: %d, Promedio: %.2f\n", est->nombre, est->edad, est->promedio);
+}
+int main() {
+    // Crear una estructura Estudiante
+    Estudiante estudiante1 = {"Juan", 20, 9.5};
+
+    // a. Llamar a la función para imprimir la estructura por valor
+    printf("Estudiante1 antes de modificar:\n");
+    imprimirEstudiante(estudiante1);
+
+    // b. Llamar a la función para modificar la estructura por referencia
+    modificarEstudiante(&estudiante1);
+
+    // Imprimir la estructura después de la modificación
+    printf("Estudiante1 después de modificar la edad:\n");
+    imprimirEstudiante(estudiante1);
+
+    // c. Llamar a la función para imprimir la estructura por dirección
+    printf("Estudiante1:\n");
+    imprimirEstudiante2(&estudiante1);
+
+    return 0;
 }
